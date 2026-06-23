a06b6ae70ec7a77619f66853f3cda92034d77c3f
Max Kadel <max@notch8.com>
2026-06-23 15:41:14 +0200

    Use released version of iiif_print

--- bundler.d/iiif_print_override.rb
+++ bundler.d/iiif_print_override.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-override_gem 'iiif_print', git: 'https://github.com/notch8/iiif_print.git', branch: 'iiif_derivatives_with_valkyrie_cache_buster'
+override_gem 'iiif_print', '~> 3.1.1'
