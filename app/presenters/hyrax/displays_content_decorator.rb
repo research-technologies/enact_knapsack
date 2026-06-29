8c8c6992f8dddd859ad42beba2c841e7cb936fa0
Kirk Wang <k3wang@gmail.com>
2026-06-29 13:23:41 -0700

    🧹 Override Hyrax::DisplaysContent#audio_content
    This override will hardcode `https://` for the audio thumbnail URL,
    otherwise CloverIIIF doesn't seem to find it.  Maybe consider adding
    this back to Hyrax.

---
+++ app/presenters/hyrax/displays_content_decorator.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# OVERRIDE Hyrax v5.2.0 to add https to the thumbnail for audio content.
+
+module Hyrax
+  module DisplaysContentDecorator
+    def audio_content
+      IIIFManifest::V3::DisplayContent.new(
+        download_path('mp3'),
+        label: 'mp3',
+        duration: conformed_duration,
+        type: 'Sound',
+        # I think UV has a bug where if it's 'audio/mpeg' then it would load, so adding this
+        # workaround to use 'audio/mp3' (which isn't even an official MIME type).
+        format: Hyrax.config.iiif_av_viewer == :universal_viewer ? 'audio/mp3' : mime_type,
+        thumbnail: [{
+          id: "https://#{hostname}/assets/audio.png",
+          type: 'Image',
+          format: 'image/png'
+        }]
+      )
+    end
+  end
+end
+
+Hyrax::DisplaysContent.prepend(Hyrax::DisplaysContentDecorator)
