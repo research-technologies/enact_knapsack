69a8284e56ab75e3e69f06e71a11fb3724176176
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2026-07-13 18:34:19 +0100

    remove commented out code for lint

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -18,16 +18,13 @@ RSpec.describe Portfolio do
     it { is_expected.to respond_to(:portfolio_identifier) }
     it { is_expected.to respond_to(:keyword) }
     it { is_expected.to respond_to(:research_group) }
-#    it { is_expected.to respond_to(:rights_statement) }
     it { is_expected.to respond_to(:file_access_level) }
     it { is_expected.to respond_to(:ref_unit_of_assessment) }
   end
 
   describe 'schema-driven compound attributes' do
     it { is_expected.to respond_to(:contributors) }
-#    it { is_expected.to respond_to(:identifiers) }
     it { is_expected.to respond_to(:funding_references) }
-#    it { is_expected.to respond_to(:organisational_units) }
     it { is_expected.to respond_to(:rights) }
   end
 
@@ -42,7 +39,6 @@ RSpec.describe Portfolio do
 
     it 'persists and reloads compound hash entries' do
       work.title = ['Test portfolio']
-    #  work.rights_statement = 'Metadata licensed CC0.'
       work.contributors = [contributor]
       work.rights = [rights]
 
@@ -51,7 +47,6 @@ RSpec.describe Portfolio do
 
       expect(reloaded.contributors.map { |c| c['given_name'] }).to contain_exactly('Avery')
       expect(reloaded.contributors.first['role_label']).to eq('composer')
-#      expect(reloaded.rights.first['rights_statment']).to eq('http://rightsstatements.org/vocab/InC-NC/1.0/')
       expect(reloaded.rights.first['license']).to eq('https://creativecommons.org/licenses/by/4.0/')
     end
   end
